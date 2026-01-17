4d51f11086c1d68646dbe1cee2254798b257adb9
Garry Marshall <107933909+Garry-Marshall@users.noreply.github.com>
2026-01-17 23:06:04 +0100

    add install requirements

--- Update.bat
+++ Update.bat
@@ -1,3 +1,10 @@
+@echo off
 call venv\Scripts\activate.bat
+echo Pulling latest changes from git...
 git pull
+echo.
+echo Installing/updating dependencies...
+pip install -r requirements.txt
+echo.
+echo Update complete!
 pause
